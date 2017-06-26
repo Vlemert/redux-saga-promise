96412ee9eb386da0ee59f64748f3393d1cb8ceab
Niels Vleeming <niels.vleeming@greenhousegroup.com>
2017-06-26 20:49:35 +0200

    Add some more race test cases

--- src/utils/__tests__/race.js
+++ src/utils/__tests__/race.js
@@ -14,6 +14,15 @@ describe('race', () => {
     expect(raceResult).toEqual('1');
   });
 
+  test('rejects if any of the promises rejects in an array', async () => {
+    const racePromise = race([
+      Promise.reject('test exception'),
+      Promise.resolve('1'),
+    ]);
+
+    expect(racePromise).rejects.toBe('test exception');
+  });
+
   test('resolves a dictionary of promises with only the winning result set', async () => {
     const racePromise = race({
       'a': Promise.resolve('1'),
@@ -25,5 +34,14 @@ describe('race', () => {
     expect(raceResult).toEqual({
       'a': '1',
     });
+  });
+
+  test('rejects if any of the promises rejects in a dictionary', async () => {
+    const racePromise = race({
+      'a': Promise.reject('test exception'),
+      'b': Promise.resolve('1'),
+    });
+
+    expect(racePromise).rejects.toBe('test exception');
   });
 });
